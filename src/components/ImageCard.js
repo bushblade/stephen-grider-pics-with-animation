83375fee7c11dab7c35ad61a27b3772704897087
Will Adams <bushblade@gmail.com>
2019-04-21 18:48:12 +0100

    margin

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,7 +3,7 @@ import { animated } from 'react-spring'
 
 const ImageCard = props => {
   return (
-    <animated.div style={{ ...props.styles }}>
+    <animated.div style={{ margin: '5px', ...props.styles }}>
       <img src={props.urls.regular} alt={props.description} />
     </animated.div>
   )
