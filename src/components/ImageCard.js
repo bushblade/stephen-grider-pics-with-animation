525edb9f861a0baccd16cc57271cf5d11bd1d7c1
Will Adams <bushblade@gmail.com>
2019-04-21 18:17:31 +0100

    clean up

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,23 +1,10 @@
-import React, { useState, useEffect, createRef } from 'react'
+import React from 'react'
 import { animated } from 'react-spring'
 
 const ImageCard = props => {
-  // const [spans, set] = useState(0)
-  const imageRef = createRef()
-
-  // const setSpans = () => {
-  //   const height = imageRef.current.clientHeight
-  //   const spans = Math.ceil(height + 10)
-  //   set(spans)
-  // }
-
-  // useEffect(() => {
-  //   imageRef.current.addEventListener('load', setSpans)
-  // }, [])
-
   return (
     <animated.div style={{ ...props.styles }}>
-      <img ref={imageRef} src={props.urls.regular} alt={props.description} />
+      <img src={props.urls.regular} alt={props.description} />
     </animated.div>
   )
 }
