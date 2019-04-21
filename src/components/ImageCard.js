89fb621448f4b74bfbf920d7f13e149591faa484
Will Adams <bushblade@gmail.com>
2019-04-21 11:09:01 +0100

    spring animation

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect, createRef } from 'react'
+import { animated, useSpring, config } from 'react-spring'
 
 const ImageCard = props => {
   const [spans, set] = useState(0)
+  const [loaded, setLoaded] = useState(false)
   const imageRef = createRef()
+
+  const spring = useSpring({
+    opacity: loaded ? 1 : 0,
+    transform: `scale(${loaded ? 1 : 0})`,
+    from: {
+      opacity: 0,
+      transform: `scale(0)`
+    },
+    config: config.stiff
+  })
 
   const setSpans = () => {
     const height = imageRef.current.clientHeight
     const spans = Math.ceil(height + 10)
     set(spans)
+    setLoaded(true)
   }
 
   useEffect(() => {
@@ -15,9 +28,9 @@ const ImageCard = props => {
   }, [])
 
   return (
-    <div style={{ gridRowEnd: `span ${spans}` }}>
+    <animated.div style={{ gridRowEnd: `span ${spans}`, ...spring }}>
       <img ref={imageRef} src={props.urls.regular} alt={props.description} />
-    </div>
+    </animated.div>
   )
 }
 
